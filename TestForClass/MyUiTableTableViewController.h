4569261ed9c2a4863ab6a5df92ffa7c73e140318
Edisonthk <edisonthk@gmail.com>
2014-04-13 01:55:57 +0900

    4

---
+++ TestForClass/MyUiTableTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  MyUiTableTableViewController.h
+//  TestForClass
+//
+//  Created by Edisonthk on 4/13/14.
+//  Copyright (c) 2014 Edisonthk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyUiTableTableViewController : UITableViewController
+
+-(IBAction)unwindToList:(UIStoryboardSegue *)segues;
+
+@end
---
+++ TestForClass/MyViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyViewController.h
+//  TestForClass
+//
+//  Created by Edisonthk on 4/13/14.
+//  Copyright (c) 2014 Edisonthk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyViewController : UIViewController
+
+@end
